642c49dfa96622a1cbf79fd06c7bfbfaf4956ed6
Muhammad Shahzil <mshahzil56@gmail.com>
2024-05-11 16:44:45 +0500

    COUNTDOWN WORD

---
+++ app.ts
@@ -0,0 +1,26 @@
+import inquirer from "inquirer"
+
+
+const answers: {
+    sentence: "string",   
+} = await inquirer.prompt([
+
+{
+name: "sentence",
+type: "input",
+message: "Enter your sentence to count the words:"
+
+
+}
+
+])
+const words = answers.sentence.split(" ");
+console.log(words)
+console.log(` your sentence word count is ${words.length}`);
+
+
+    
+   
+
+
+    
